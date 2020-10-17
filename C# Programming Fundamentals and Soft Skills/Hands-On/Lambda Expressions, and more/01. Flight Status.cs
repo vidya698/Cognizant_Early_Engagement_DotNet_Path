cc2be195f0414f3cdd0d365a3ede5fe5cd20eb3a
Ujwal Mahindrakar <ujwalmahindrakar4@gmail.com>
2020-10-17 23:47:41 +0530

    Added Flight Status C# Hands-On

---
+++ C# Programming Fundamentals and Soft Skills/Hands-On/Lambda Expressions, and more/01. Flight Status.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateEx2		//DO NOT CHANGE namespace name
+{
+	public class Program	//DO NOT CHANGE the class name
+	{
+		/*** Dictionary values are hard-coded. DO NOT change ***/
+		static Dictionary<string, DateTime> flightSchedule = new Dictionary<string, DateTime>(){
+														{"ZW346", Convert.ToDateTime("13:54:10")},
+														{"AT489", Convert.ToDateTime("16:30:00")},
+														{"BR267", Convert.ToDateTime("21:15:30")}};
+		
+		public class void Main(string[] args)	//DO NOT CHANGE the 'Main' method signature
+		{
+			//Implement your code here
+			string flightNumber;
+			string timeLeft;
+			
+			Console.WriteLine("Enter the Flight Number :");
+			flightNumber = Console.ReadLine();
+			
+			timeLeft = flightStatus(flightNumber);
+			Console.WriteLine(timeLeft);
+		}
+		
+		public static string flightStatus(string flightNo)	//DO NOT CHANGE the 'flightStatus' method signature
+		{
+			//Implement your code here
+			if(flightSchedule.ContainsKey(flightNo))
+			{
+				DateTime departureTime = flightSchedule[flightNo];
+				if(DateTime.UtcNow < departureTime)
+				{
+					TimeSpan ts = departureTime.Subtract(DateTime.UtcNow);
+					return "Time to flight " + ts.ToString();
+				}
+				else
+					return "Flight Already Left";
+			}
+			else
+				return "Invalid Flight Number";
+		}
+	}
+}
+	
